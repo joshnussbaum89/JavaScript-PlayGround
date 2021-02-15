0e5ccc9688e36b82fc14af6f7cae2f7abb028e36
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-02-15 09:14:31 -0600

    diagonal difference

--- main.js
+++ main.js
@@ -338,6 +338,33 @@ const plusMinus = arr => {
 };
 
 
+
+/**
+ * Diagonal Difference
+ @param {arr} array of arrays 
+ @returns absolute difference between the sums of its diagonals
+ ***************************************************************/
+
+const diagonalDifference = arr => {
+  const arrLength = arr.length;
+  let diag1 = 0;
+  let diag2 = 0;
+
+  for (let i = 0; i < arrLength; i++) {
+    for (let j = 0; j < arrLength; j++) {
+      if (i === j) {
+        diag1 += arr[i][j];
+      }
+      if (i + j === arrLength - 1) {
+        diag2 += arr[i][j];
+      }
+    }
+  }
+
+  return Math.abs(diag1 - diag2);
+}
+
+
 //Test function: insert test functions here
 function test() {
 
