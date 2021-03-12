67f6abda29b4a44105042569861d49d1e2f2f9fc
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-03-12 09:50:47 -0600

    detect browser function

--- main.js
+++ main.js
@@ -512,9 +512,31 @@ function gradingStudents(grades) {
   return roundedGrades;
 }
 
+
+
+/**
+ * Detect Browser
+ @param {string} string 
+ @returns Browser name
+ ***************************************************************/
+
+function detectBrowser(userAgent) {
+  if (userAgent.includes('Chrome')) {
+    return 'Google Chrome';
+  } else if (userAgent.includes('Firefox')) {
+    return 'Mozilla Firefox';
+  } else {
+    return 'Internet Explorer';
+  }
+}
+
 //Test function: insert test functions here
 function test() {
-  console.log(gradingStudents([73, 67, 38, 33]));
+  console.log(
+    detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"), // ➞ "Google Chrome"
+    detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"), // ➞ "Mozilla Firefox"
+    detectBrowser("Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko") // ➞ "Internet Explorer"
+  )
 }
 
 test();
