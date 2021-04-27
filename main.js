b47c6544dd1f137975e15c2772659dd024f0e9cf
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-04-27 10:28:29 -0500

    distance traveled up stairs

--- main.js
+++ main.js
@@ -730,12 +730,29 @@ const hasSyncopation = (s) => {
   });
 };
 
+/**
+ * Snail Goes Up The Stairs
+ * @param {*} height 
+ * @param {*} length 
+ * @param {*} tower 
+ * @returns The distance one would travel going up a starcase
+ */
+const totalDistance = (height, length, tower) => {
+  const numberOfStairs = tower / height;
+  const distanceForEachStair = height + length;
+  const totalDistanceTraveled = +(
+    numberOfStairs * distanceForEachStair
+  ).toFixed(1);
+
+  return totalDistanceTraveled;
+};
+
 // Test function: insert test functions here
 function test() {
   console.log(
-    hasSyncopation(".#.#.#.#"), // ➞ true
-    hasSyncopation("#.#...#."), // ➞ false
-    hasSyncopation("#.#.###.") // ➞ true
+    totalDistance(0.2, 0.4, 100.0), // ➞ 300.0
+    totalDistance(0.3, 0.2, 25.0), // ➞ 41.7
+    totalDistance(0.1, 0.1, 6.0) // ➞ 12.0);
   );
 }
 
