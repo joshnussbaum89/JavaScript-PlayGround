12b5c692165a0cb8ad36c7ad70583851e044602d
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-06-14 16:54:54 -0500

    count farm legs & return first item in array

--- index.php
+++ index.php
@@ -18,4 +18,29 @@ function convert($minutes)
     return $minutes * 60;
 }
 
+/**
+ * Count total of farm animal legs
+ */
+function animals($chickens, $cows, $pigs)
+{
+    // calculate legs of each animal
+    $chickenLegs = $chickens * 2;
+    $cowLegs = $cows * 4;
+    $pigLegs = $pigs * 4;
 
+    // calculate leg total
+    $legTotal = $chickenLegs + $cowLegs + $pigLegs;
+
+    return $legTotal;
+}
+
+function getFirstValue($array)
+{
+    return $array[0];
+}
+
+echo getFirstValue([1, 2, 3]); // ➞ 1
+
+echo getFirstValue([80, 5, 100]); // ➞ 80
+
+echo getFirstValue([-500, 0, 50]); // ➞ -500
